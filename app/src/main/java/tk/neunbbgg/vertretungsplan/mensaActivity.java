f35cb22f87e0027b0502c0aab0e542142be34927
philipp <philippharms1@web.de>
2016-01-31 19:56:54 +0100

    added haplan

--- app/src/main/java/tk/neunbbgg/vertretungsplan/mensaActivity.java
+++ app/src/main/java/tk/neunbbgg/vertretungsplan/mensaActivity.java
@@ -36,7 +36,7 @@ public class mensaActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Sinnloser Knopf", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
