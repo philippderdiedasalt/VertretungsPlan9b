e9e69fa8a26dee6259a1804f1b84338664b0d9a1
philipp <philippharms1@web.de>
2016-02-02 18:57:06 +0100

    fixed source

--- app/src/main/java/tk/neunbbgg/vertretungsplan/davidActivity.java
+++ app/src/main/java/tk/neunbbgg/vertretungsplan/davidActivity.java
@@ -6,16 +6,17 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 
 public class davidActivity extends AppCompatActivity {
-
+ImageView david;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_david);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        david = (ImageView) findViewById(R.id.david);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
