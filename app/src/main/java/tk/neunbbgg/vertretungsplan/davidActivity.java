1e8ebb9944d9097bc20b2adc54612d9edbfec0be
philipp <philippharms1@web.de>
2016-02-02 19:15:49 +0100

    fixed source

--- app/src/main/java/tk/neunbbgg/vertretungsplan/davidActivity.java
+++ app/src/main/java/tk/neunbbgg/vertretungsplan/davidActivity.java
@@ -17,10 +17,12 @@ ImageView david;
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         david = (ImageView) findViewById(R.id.david);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
