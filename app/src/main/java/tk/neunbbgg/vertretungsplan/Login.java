651da83ebed33e58bc735b941be48e12e28bbb75
philipp <philippharms1@web.de>
2016-03-22 11:23:03 +0100

    add login server script

--- app/src/main/java/tk/neunbbgg/vertretungsplan/Login.java
+++ app/src/main/java/tk/neunbbgg/vertretungsplan/Login.java
@@ -1,9 +1,6 @@
 package tk.neunbbgg.vertretungsplan;
 
 import android.app.AlertDialog;
-import android.app.admin.DeviceAdminInfo;
-import android.app.admin.DevicePolicyManager;
-import android.bluetooth.BluetoothClass;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -30,25 +27,18 @@ import org.json.JSONObject;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.io.PrintStream;
 import java.io.PrintWriter;
-import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.UnknownHostException;
-import java.security.Security;
-
-import static java.security.AccessController.getContext;
 
 public class Login extends ActionBarActivity implements View.OnClickListener {
     public static final String DEFAULT = "N/A";
